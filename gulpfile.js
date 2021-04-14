19bcd55ffa83109d1a62dce7f14fe305797f5d13
Elizabeth Hellsten <elizabeth_hellsten@shaw.ca>
2021-04-14 16:22:26 -0500

    Update gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -2,49 +2,46 @@ const { src, dest, series, parallel } = require('gulp');
 const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer');
-const sourcemaps = require('gulp-sourcemaps'); 
+const sourcemaps = require('gulp-sourcemaps');
 const concat = require('gulp-concat');
+const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
-const watch = require('gulp-watch');
 
 function htmlTask() {
   return src('src/*.html')
     .pipe(dest('dist'))
 }
 
-function scriptsTask() {
-  return src('src/*.js')
-    .pipe(uglify())
-    .pipe(dest('dist'))
-}
-
 function stylesTask() {
-  return src('src/*.css')
+  return src('src/css/*.css')
     .pipe(sourcemaps.init())
     .pipe(postcss([autoprefixer(), cssnano()]))
     .pipe(sourcemaps.write())
-    .pipe(dest('dist'))
+    .pipe(concat('style.css'))
+    .pipe(dest('dist/css'))
 }
 
-function imagesTask() {
-  return src('src/*.png')
+function scriptsTask() {
+  return src('src/js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(concat('app.js'))
+    .pipe(dest('dist/js'))
+}
+
+function imagestask() {
+  return src('src/images/*')
     .pipe(imagemin())
-    .pipe(dest('dist'))
-}
-
-function watchTask() {
-  return watch('src/*', function() {
-    src('src/*.css')
-    .pipe(dest('dist'))
-  })
-    
+    .pipe(dest('dist/images'))
 }
 
 exports.html = htmlTask;
-exports.scripts = scriptsTask;
 exports.styles = stylesTask;
-exports.images = imagesTask;
-exports.watch = watchTask;
-
-exports.default = series(htmlTask, scriptsTask, stylesTask, imagesTask);
+exports.scripts = scriptsTask;
+exports.images = imagestask;
+exports.default = parallel(htmlTask, stylesTask, scriptsTask, imagestask);
